703bd12f5ef4c7cd22e643db6116d6d4f9087cf2
OnlyMaxon <calilorucli42@gmail.com>
2026-03-19 04:23:36 +0100

    feat: add mobile overrides and iOS Google webview fallbacks for improved presentation and typography

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { LanguageProvider, AuthProvider } from './context'
 import './styles/globals.css'
+import './styles/mobile-overrides.css'
+
+const ua = window.navigator.userAgent
+const isIOS = /iPhone|iPad|iPod/i.test(ua)
+const isGoogleShell = /GSA|CriOS/i.test(ua)
+
+if (isIOS && isGoogleShell) {
+  document.documentElement.classList.add('ios-google')
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
