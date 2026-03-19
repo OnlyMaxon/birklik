062f90baaa87a05c802c14447f8aeec8a67e5ff7
OnlyMaxon <calilorucli42@gmail.com>
2026-03-19 02:25:12 +0100

    fix: update favicon and marker icons for improved visual consistency

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -4,14 +4,17 @@ import L from 'leaflet'
 import { useLanguage } from '../../context'
 import { Property, Language } from '../../types'
 import 'leaflet/dist/leaflet.css'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 import './Map.css'
 
 // Fix for default markers in react-leaflet
 delete (L.Icon.Default.prototype as any)._getIconUrl
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
 })
 
 interface PropertyMapProps {
