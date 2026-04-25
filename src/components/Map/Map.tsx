df5e4adbcf8347d3bc5c36605d9848cc8b9b031b
OnlyMaxon <calilorucli42@gmail.com>
2026-04-25 02:21:42 +0200

    fix(Map): update TileLayer URL to use voyager raster tiles

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -81,7 +81,7 @@ export const PropertyMap: React.FC<PropertyMapProps> = ({
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         />
         {properties.map((property) => (
           <Marker 
